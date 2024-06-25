824acf575a9ef6b15446efe7cc77a1fa33ff8e85
ItzD <itzeeldaniela@gmail.com>
2024-06-24 21:59:29 -0600

    Aluraflix: proyecto base

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from "./componentes/Header/index"
+import Home from "./componentes/Home/index"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Home/index.js
@@ -0,0 +1,10 @@
+import "./Home.css"
+
+function Home(){
+    return <div className="home">
+        <img src="./img/Portada-alura.png" alt="portadaAlura" />
+    </div>
+    
+}
+
+export default Home
